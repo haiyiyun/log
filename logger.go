3699970552a6e3228e5f5261c7ab0bac8ab77c16
leetaifook <lee@leetaifook.com>
2022-01-25 12:04:29 +0800

    add Ldevelop

--- logger.go
+++ logger.go
@@ -16,16 +16,17 @@ const (
 	// order they appear (the order listed here) or the format they present (as
 	// described in the comments).  A colon appears after these items:
 	//	[log] 2009/01/23 01:23:23.123123 /a/b/c/d.go:23: message
-	Ldate         = log.Ldate                                                             // the date: 2009/01/23
-	Ltime         = log.Ltime                                                             // the time: 01:23:23
-	Lmicroseconds = log.Lmicroseconds                                                     // microsecond resolution: 01:23:23.123123.  assumes Ltime.
-	Llongfile     = log.Llongfile                                                         // full file name and line number: /a/b/c/d.go:23
-	Lshortfile    = log.Lshortfile                                                        // final file name element and line number: d.go:23. overrides Llongfile
-	LUTC          = log.LUTC                                                              // if Ldate or Ltime is set, use UTC rather than the local time zone
-	Lpackage      = log.LUTC << 1                                                         // package name: [log]
-	Lfunction     = Lpackage << 1                                                         // function name: <Print>
-	LstdFlags     = log.LstdFlags | log.Lmicroseconds | Lshortfile | Lpackage | Lfunction // initial values for the standard logger
-	LProduction   = log.LstdFlags | log.Lmicroseconds | Lshortfile
+	Ldate         = log.Ldate                                                         // the date: 2009/01/23
+	Ltime         = log.Ltime                                                         // the time: 01:23:23
+	Lmicroseconds = log.Lmicroseconds                                                 // microsecond resolution: 01:23:23.123123.  assumes Ltime.
+	Llongfile     = log.Llongfile                                                     // full file name and line number: /a/b/c/d.go:23
+	Lshortfile    = log.Lshortfile                                                    // final file name element and line number: d.go:23. overrides Llongfile
+	LUTC          = log.LUTC                                                          // if Ldate or Ltime is set, use UTC rather than the local time zone
+	Lpackage      = log.LUTC << 1                                                     // package name: [log]
+	Lfunction     = Lpackage << 1                                                     // function name: <Print>
+	LstdFlags     = Ldate | Ltime | Lmicroseconds | Lshortfile | Lpackage | Lfunction // initial values for the standard logger
+	Ldevelop      = Ldate | Ltime | Lmicroseconds | Llongfile | Lpackage | Lfunction
+	Lproduction   = Ldate | Ltime | Lmicroseconds | Lpackage | Lfunction
 )
 
 //日志级别
